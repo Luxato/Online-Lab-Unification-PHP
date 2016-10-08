48703a9824be002bc1e2b7962aff4fd7b4afa51d
Lukas Stranovsky <lukas.stranovsky@us-spider.com>
2016-10-08 20:15:04 +0200

    Init laravel

---
+++ laravel/resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('master') {{--rozsirujem sablonu master, cize tato sekcia sa ijectne do mastra--}}
+
+@section('content')
+    <div class="content">
+        <h1>Toto je nadpis z welcome.blade.php</h1>
+    </div>
+@stop
+
+@section('title','Online lab unification');
