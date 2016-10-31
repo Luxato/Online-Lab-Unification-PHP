98426b7ac0ac08c96cec47a89a0cb329694778c5
Lukas Stranovsky <lukas.stranovsky@us-spider.com>
2016-10-31 20:28:50 +0100

    Add login to navigation

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -84,6 +84,45 @@
                         </li>
                     </ul>
                 </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Login</b> <span class="caret"></span></a>
+                        <ul id="login-dp" class="dropdown-menu">
+                            <li>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        {{--Login via
+                                        <div class="social-buttons">
+                                            <a href="#" class="btn btn-fb"><i class="fa fa-facebook"></i> Facebook</a>
+                                            <a href="#" class="btn btn-tw"><i class="fa fa-twitter"></i> Twitter</a>
+                                        </div>
+                                        or--}}
+                                        <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
+                                            <div class="form-group">
+                                                <label class="sr-only" for="exampleInputEmail2">Email address</label>
+                                                <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email address" required="">
+                                            </div>
+                                            <div class="form-group">
+                                                <label class="sr-only" for="exampleInputPassword2">Password</label>
+                                                <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" required="">
+                                                <div class="help-block text-right"><a href="">Forget the password ?</a></div>
+                                            </div>
+                                            <div class="form-group">
+                                                <button type="submit" class="btn btn-primary btn-block">Sign in</button>
+                                            </div>
+                                            <div class="checkbox">
+                                                <label>
+                                                    <input type="checkbox"> keep me logged-in
+                                                </label>
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <div class="bottom text-center">
+                                        New here ? <a href="#"><b>Join Us</b></a>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </li>
             </ul>
         </div><!--/.nav-collapse -->
 <ul class="translation-flags">
@@ -131,6 +170,18 @@
 <script>
     $(window).load(function () {
         $(".se-pre-con").fadeOut("slow");
+    });
+    $(window).scroll(function() {
+        var navbar = $(".navbar-fixed-top");
+        if (navbar.offset().top > 50) {
+            navbar.stop().animate({
+                'opacity': 0
+            },500);
+        } else {
+            navbar.stop().animate({
+                'opacity': 1
+            },500);
+        }
     });
 </script>
 <div class="se-pre-con"></div>
