0104b4e82a8aafa488963e73e52c60aedc8fe518
Lukas Stranovsky <lukas.stranovsky@us-spider.com>
2017-04-17 22:10:54 +0200

    Fix duplicated paths in reordering nav

--- app/Http/Controllers/Admin.php
+++ app/Http/Controllers/Admin.php
@@ -17,7 +17,23 @@ class Admin extends Controller {
 	public function navigation() {
 		$this->init_navigation();
 		$data['navigation'] = $this->navigation;
-
+		$ids = [];
+		foreach ($data['navigation'] as $key1 => $page) {
+			$ids[] = $page->page_id;
+			if (isset($page->children)) {
+			    foreach($page->children as $key2 => $subpage) {
+				    $ids[] = $subpage->page_id;
+				        if (isset($subpage->children)) {
+					        foreach($subpage->children as $key3 => $subsubpage) {
+					        	if (in_array($subsubpage->page_id, $ids)) {
+					        	    unset($data['navigation'][$key1]->children[$key2]->children[$key3]);
+					        	}
+						        $ids[] = $subsubpage->page_id;
+					        }
+				        }
+			    }
+			}
+		}
 		return view( 'administration/navigation', $data );
 	}
 
